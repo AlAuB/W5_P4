ba2228da4cb3143df0f441f2c03c02f27d477b8f
Jinpeng Lyu <Louis201307@outlook.com>
2022-10-13 00:24:29 -0400

    V0.9 Checking Bugs

--- app/src/main/java/com/example/w5_p4/GameFrame.java
+++ app/src/main/java/com/example/w5_p4/GameFrame.java
@@ -265,7 +265,7 @@ public class GameFrame extends Fragment implements View.OnClickListener {
 
     private void submit() {
         String word = input.getText().toString().toLowerCase();
-        if (wordCheck(word) && word.length() > 0) {
+        if (wordCheck(word) && !word.equals("input shows here")) {
             if (!answered.contains(word)) {
                 if (dictionary.contains(word)) {
                     answered.add(word);
